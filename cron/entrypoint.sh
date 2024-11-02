a3086aca96288d77fd4609f3783afeeec6681304
Dung Le <xdung24@gmail.com>
2024-11-02 10:22:44 +0700

    Remove cron service

--- cron/entrypoint.sh
+++ cron/entrypoint.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 
-cron && tail -f /home/nonroot/cron.log
+# run cron daemon in the background
+cron && \ 
+tail -f /home/nonroot/cron.log # watch the log file
+
+# In production we can run a web server or other service here instead of tailing the log file
--- cron/script.sh
+++ cron/script.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 me=$(basename "$0")
-echo "Hello world from ${me} with user $(whoami) at $(date)" >> cron.log
+echo "Hello world from ${me} with user $(whoami) at $(date)" >> /home/nonroot/cron.log
