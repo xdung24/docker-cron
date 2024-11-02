1e491b48e9ef6ac8c7be147fae8ef07c33aad978
Dung Le <xdung24@gmail.com>
2024-11-02 09:58:02 +0700

    run cron as nonroot user

--- cron/script.sh
+++ cron/script.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-echo "Hello world from script with user" $(whoami) " at " $(date) >> cron.log
+me=$(basename "$0")
+echo "Hello world from ${me} with user $(whoami) at $(date)" >> cron.log
